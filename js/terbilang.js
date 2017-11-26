55a8edea9fba8493c5973b74fe193a4c6e92d819
titounnes <zano.amrhakim@gmail.com>
2017-11-26 14:02:08 +0700

    fix bug pada penyebutan 100000 dan sejenisnya

--- js/terbilang.js
+++ js/terbilang.js
@@ -1,63 +1,63 @@
-//mendefinisikan satuan 
-var units = [' ',  'ribu ', 'juta', 'milyar',  'triliun', 'biliun'];
-//Mendefinisikan bilangan
-var angka = ["", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"];
-//membuat function untuk memecah bilangan menjadi array beranggota 3 digit
 var digit3 = function(feed) {
-  
+
+  //mendefinisikan satuan 
+  var units = ['', 'ribu ', 'juta', 'milyar', 'triliun', 'biliun'];
+  //Mendefinisikan bilangan
+  var angka = ["", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"];
+  //membuat function untuk memecah bilangan menjadi array beranggota 3 digit
+
+
   //menginisiasi luaran
   var output = '';
-  
+
   if (feed.length % 3 > 0) {
     feed = '00'.substr(0, (3 - feed.length % 3)) + feed;
     //1234 akan diubah menjadi 001234
   }
-  //menyisipkan titik sebagai separator 
+
+  //menyisipkan titik sebagai separator
   //001234 akan diubah menjadi 001.123
   while (/(\d+)(\d{3})/.test(feed)) {
     feed = feed.replace(/(\d+)(\d{3})/, '$1' + '.' + '$2');
   }
 
-	var segment3 = feed.split('.'); 
+  var segment3 = feed.split('.');
   //Membilang setiap segmen 3 digit
-  $.each(segment3, function(i, v){
-  	//memecah 3 digit menjadi arrau 1 digit
-    var digit = v.split('');
-    //menentukan nilai ratusan
-    if(digit[0] == '1'){
-    	output += 'seratus ';
-    }else if(digit[0] != '0'){
-   		output += angka[digit[0]] + ' ratus ';  	
-    }
-    //menentukan nilai puluhan
-    if(digit[1] == '1'){
-    	if(digit[2] == '0'){
-      	output += 'sepuluh ';
-      }else{
-      	output += angka[digit[2]]+ 'belas '; 
-      }          
-    }else if(digit[1] != '0'){
-    	output += angka[digit[1]] +' puluh ' + angka[digit[2]]+' ';
-    }else{
-    	if(digit[0] == '0' && digit[1]=='0' && digit[2]=='1'){
-      	output += 'se';
-      }else{
-      	output += angka[digit[2]]+ ' ';
+  $.each(segment3, function(i, v) {
+    if (v * 1 != 0) {
+      //memecah 3 digit menjadi arrau 1 digit
+      var digit = v.split('');
+
+      //menentukan nilai ratusan
+      output += digit[0] == '1' ?
+        'seratus' :
+        (digit[0]!='0' ? angka[digit[0]] + ' ratus ' : '');
+
+      //menentukan nilai puluhan
+      if (digit[1] == '1') {
+        output += (digit[2] == '0') ? 'sepuluh ' : angka[digit[2]] + 'belas ';
+      } else if (digit[1] != '0') {
+        output += angka[digit[1]] + ' puluh ' + angka[digit[2]] + ' ';
+      } else {
+        if (digit[0] == '0' && digit[1] == '0' && digit[2] == '1') {
+          output += (i == segment3.length - 1) ? 'se' : 'satu ';
+        } else {
+          output += angka[digit[2]] + ' ';
+        }
       }
+      output += units[segment3.length - i - 1] + ' ';
     }
-    output += units[segment3.length-i-1]+' ';
   })
   return output;
 }
 
 
-$(document).on('click','#converter', function(){
-	var feed = $('#number').val();
-	if(isNaN(feed)){
-  	alert('Yang anda tulis bukan bilangan')
+$(document).on('click', '#converter', function() {
+  var feed = $('#number').val();
+  if (isNaN(feed)) {
+    alert('Yang anda tulis bukan bilangan')
     return false;
   }
-  
-  var digits =  
-  $('#output').html(digit3(feed))
+
+  var digits = $('#output').html(digit3(feed))
 })
